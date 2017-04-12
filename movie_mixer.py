93d06bdde296983ad53fc9de10a0755f76eb1e37
shahin <shahin@shahins-MacBook-Pro.local>
2017-04-12 11:21:53 +0200

    new istances added to movie mixer

--- movie_mixer.py
+++ movie_mixer.py
@@ -25,9 +25,24 @@ lord_of_rings_1 = mediatek.Movie("The Lord of the Rings: The Fellowship of the R
                                  "http://static.rogerebert.com/uploads/movie/movie_poster/lord-of-the-rings-the-fellowship-of-the-ring-2001/large_9HG6pINW1KoFTAKY3LdybkoOKAm.jpg",
                                  "https://www.youtube.com/watch?v=V75dMMIW2B4")
 
+
+forrest_gump = mediatek.Movie ("Forrest Gump",
+                               "https://s-media-cache-ak0.pinimg.com/736x/cd/d9/e3/cdd9e3b6c6072fb0f8c6a54bfc4bc7a4.jpg",
+                               "https://www.youtube.com/watch?v=uPIEn0M8su0")
+
+lord_of_rings_2 = mediatek.Movie("The Lord of the Rings: Two Towers",
+                                 "http://www.gstatic.com/tv/thumb/movieposters/30793/p30793_p_v8_am.jpg",
+                                 "https://www.youtube.com/watch?v=2dlRvAjU_RI")
+
+goodfellas = mediatek.Movie("Goodfellas",
+                            "http://static.rogerebert.com/uploads/movie/movie_poster/goodfellas-1990/large_pDTuWp3jRcGbfWn0Ic6XZ0M0DwP.jpg",
+                            "https://www.youtube.com/watch?v=qo5jJpHtI1Y")
+
+                            
                                  
 
-movies=[shawshank_redemption, godfather_2, twelve_angry_men, pulp_fiction, good_bad_ugly, lord_of_rings_1]
+movies=[shawshank_redemption, godfather_2, twelve_angry_men, pulp_fiction, good_bad_ugly, lord_of_rings_1,
+        forrest_gump, lord_of_rings_2, goodfellas]
 fresh_tomatoes.open_movies_page(movies)
 
 print ("Class doc: " + mediatek.Movie.__doc__ +
