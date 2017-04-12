14e05d09ae6f41a5bb563bacce652a2834b49080
shahin <shahin@shahins-MacBook-Pro.local>
2017-04-12 11:05:42 +0200

    class movie in mediatek created

---
+++ mediatek.py
@@ -0,0 +1,7 @@
+class Movie():
+        """this class allows storing information about my favourite movies"""
+        def __init__(self, movie_title, movie_picture, movie_video):
+                self.title=movie_title
+                self.poster_image_url=movie_picture
+                self.trailer_youtube_url=movie_video
+
